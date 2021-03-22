4dfe1125102587f23af9a36557753bcd2ea6807b
David8823 <david.correao@udea.edu.co>
2021-03-22 11:47:01 -0500

    info final

---
+++ lab6/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int a;
+
+
+int main()
+{
+    cout<<"introdusca un numero"<<endl;
+    cin>>a;
+
+    int t=1;
+
+    for (int i=1;i<=a; i++)
+    {
+        t=t*i;
+
+    }
+
+    cout<<endl<<t<<endl;
+
+}
